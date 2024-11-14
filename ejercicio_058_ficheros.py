646fda5d075a9eb56c4073959a960d2198238703
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2024-11-14 20:29:18 +0100

    Update ejercicio_058_ficheros.py

--- ejercicio_058_ficheros.py
+++ ejercicio_058_ficheros.py
@@ -14,5 +14,13 @@ fichero.write('Hola')
 fichero.close()
 
 fichero = open('./poema.txt', mode='rt', encoding='utf-8')
-print(fichero.read()) # Todo el fichero, str
+#print(fichero.read()) # Todo el fichero, str
+#print(fichero.read(10)) # Hay dulzur, str
+#print(fichero.read(10)) # a infantil, str
+#fichero.seek(0) # Pone el puntero en la posición 0
+#print(fichero.read(10)) # Hay dulzur, str
+#print(f'#{fichero.readline()}#') # Incluye los saltos de línea
+#print(f'#{fichero.readline().replace('\n','')}#') # #En la mañana quieta.#
+#print(fichero.readlines()) # Todo, list (incluye los saltos de línea)
+#print(fichero.readlines(200)) # Devuelve líneas enteras hasta completar el número de bytes
 fichero.close()
